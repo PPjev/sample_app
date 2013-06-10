f22fa71e9831e8141c7697dffd394e7ad6f01b09
Pavel Podkopajev <ppodkopajev@gmail.com>
2013-06-10 18:12:59 +0300

    Making authentication

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -63,7 +63,11 @@ describe "User pages" do
 
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
-    before { visit edit_user_path(user) }
+    before do
+      visit signin_path
+      sign_in user
+      visit edit_user_path(user)
+    end
 
     describe "page" do
       it { should have_content("Update your profile") }
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,6 +1,6 @@
 include ApplicationHelper
 
-def valid_signin(user)
+def sign_in(user)
   fill_in "Email",    with: user.email
   fill_in "Password", with: user.password
   click_button "Sign in"
