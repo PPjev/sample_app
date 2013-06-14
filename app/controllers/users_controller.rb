235a2bb4acd509546ab142f37b9853ae21e60999
Pavel Podkopajev <ppodkopajev@gmail.com>
2013-06-14 17:52:29 +0300

    Finish redirect back-or

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ before_action :correct_user,    only: [:edit, :update]
   end
 
   def edit
+
   end
 
   def update
@@ -36,19 +37,22 @@ before_action :correct_user,    only: [:edit, :update]
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+	  def user_params
+	    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	  end
 
-  # Before filters
+	  # Before filters
 
-  def signed_in_user
-    redirect_to signin_url, notice: "Please sign in." unless signed_in?
-  end
+	  def signed_in_user
+	  	unless signed_in?
+	  		store_location
+	  		redirect_to signin_url, notice: "Please sign in."
+	  	end
+	  end
 
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_path) unless current_user?(@user)
-  end
+	  def correct_user
+	    @user = User.find(params[:id])
+	    redirect_to(root_path) unless current_user?(@user)
+	  end
 
 end
